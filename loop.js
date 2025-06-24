6ad3e76ecddc7bffbfcf4985be823f927b3ee34c
Muaaz Aamer <muaaz.amir@devsinc.com>
2025-06-25 00:19:32 +0500

    Completed arrays,loops,operators and conditional statements

---
+++ loop.js
@@ -0,0 +1,47 @@
+// for (let i =0 ;i <5 ;i++){
+//     console.log(i)
+// }
+
+
+
+// let i =0
+// while (i<5){
+//     console.log(i)
+//     i++
+// }
+
+
+
+// i=20
+// do{
+//     console.log(i)
+//     i++
+// } while (i<10);
+
+
+
+
+// str="abcde"
+// for (let i of str){
+//     console.log(i)
+// }
+// i="name"
+// let student ={
+//     name: "muaaz",
+//     age:23,
+//     pass:true, 
+//     cpg:1.9
+// }
+// for (let i in student){
+//     console.log(student[i])
+// }
+
+////template literals
+// for (let i in student){
+//     console.log(` ${i} contains ${student[i]}`)
+// }
+
+
+// str1="0123456789"
+// str3=str1.replace("01","hello")
+// console.log(str3,str1)

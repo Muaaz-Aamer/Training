d2e97fb097ebd24fee800f38d985e01e0d6ebc40
saboor-dev1 <saboor.ahmed@devsinc.com>
2025-07-02 23:34:29 +0500

    leaderboard| added logic of adding players

--- leaderboard_project/scripts/main.js
+++ leaderboard_project/scripts/main.js
@@ -12,3 +12,58 @@ class Person {
     this.score = this.score - 5;
   }
 }
+let fName;
+let inp1 = document.querySelector(".fName");
+inp1.addEventListener("input", (e) => {
+  fName = e.target.value;
+  console.log(fName);
+});
+
+let lName;
+let inp2 = document.querySelector(".lName");
+inp2.addEventListener("input", (e) => {
+  lName = e.target.value;
+  console.log(lName);
+});
+
+let country;
+let inp3 = document.querySelector(".country");
+inp3.addEventListener("input", (e) => {
+  country = e.target.value;
+  console.log(country);
+});
+
+let score;
+let inp4 = document.querySelector(".score");
+inp4.addEventListener("input", (e) => {
+  score = e.target.value;
+  console.log(score);
+});
+
+let people = [];
+
+let add_player = document.querySelector(".submit");
+add_player.addEventListener("click", () => {
+  people.push(new Person(fName, lName, country, score));
+  //   console.log(people);
+  let node = document.querySelector(".players");
+  node.innerHTML = " ";
+  people.forEach((i) => {
+    let row = document.createElement("div");
+    let p1 = document.createElement("p");
+    console.log(i.fName);
+    p1.innerText = i.fName;
+    let p2 = document.createElement("p");
+    p2.innerText = i.lName;
+    console.log(i.lName);
+    let p3 = document.createElement("p");
+    p3.innerText = i.country;
+    console.log(i.country);
+    let p4 = document.createElement("p");
+    p4.innerText = i.score;
+    console.log(i.score);
+
+    row.append(p1, p2, p3, p4);
+    node.append(row);
+  });
+});

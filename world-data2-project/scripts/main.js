7ccf78691c9c605f9e124ec69d197624f1aadab4
saboor-dev1 <saboor.ahmed@devsinc.com>
2025-07-07 14:50:35 +0500

    Added changes based on reviews

--- world-data2-project/scripts/main.js
+++ world-data2-project/scripts/main.js
@@ -1,12 +1,10 @@
 import countries from "../data/countries.js";
 console.log("HELLO");
 let check = 0;
-// console.log(countries);
 
 let btn1 = document.querySelector(".btn1");
 btn1.addEventListener("click", () => {
   check = 1;
-  console.log(check);
 });
 
 let btn2 = document.querySelector(".btn2");
@@ -17,23 +15,18 @@ btn2.addEventListener("click", () => {
 let inp = document.querySelector("input");
 inp.addEventListener("input", (e) => {
   let word = e.target.value.toLowerCase();
-  console.log(word);
   let arrC = [];
   if (check === 1) {
     let pattern = new RegExp("^" + word, "i");
-    console.log(pattern);
     for (let country of countries) {
       if (pattern.test(country)) {
-        console.log(country);
         arrC.push(country);
       }
     }
   } else if (check === 2) {
     let pattern = new RegExp(word, "i");
-    console.log(pattern);
     for (let country of countries) {
       if (pattern.test(country)) {
-        console.log(country);
         arrC.push(country);
       }
     }
